396d47d867dc01c9319f0e2101e15e41321e9e98
tzelginianc <tzelginia78@gmail.com>
2022-04-20 12:45:27 +0700

    route prak 3

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ArticleController;
 //ROUTE PRAK 1
 Route::resource('articles', ArticleController::class);
+//route untuk fungsi cetak pdf
+Route::get('/article/cetak_pdf', [ArticleController::class, 'cetak_pdf']);
